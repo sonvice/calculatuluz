efc339ee8251b9298b166120166d9bf95af251b4
sonvice <wvmc_es@hotmail.com>
2025-11-09 22:14:42 +0100

    Add Radiador eléctrico to select

--- src/constant/index.js
+++ src/constant/index.js
@@ -5,20 +5,21 @@ export const navigation = [
 ];
 
 
- export const appliances = [
-    { value: 50, label: 'Bombilla LED', watts: '50W', image: '/images/bombilla.jpg' },
-    { value: 150, label: 'Portátil', watts: '150W', image: '/images/laptop.jpg' },
-    { value: 300, label: 'Televisor 50"', watts: '300W', image: '/images/tv.jpg' },
-    { value: 800, label: 'Microondas', watts: '800W', image: '/images/microwave.jpg' },
-    { value: 900, label: 'Aire acondicionado', watts: '900W', image: '/images/aire-acondicionado.jpg' },
-    { value: 1200, label: 'Horno', watts: '1.200W', image: '/images/horno.jpg' },
-    { value: 1200, label: 'Secador de pelo', watts: '1.200W', image: '/images/hair-dryer.jpg' },
-    { value: 1800, label: 'Lavaplatos', watts: '1.800W', image: '/images/lavaplatos.jpg' },
-    { value: 2000, label: 'Lavadora', watts: '2.000W', image: '/images/washing-machine.jpg' },
-    { value: 'custom', label: 'Personalizado', watts: '', image: '/images/personalizado.jpg' },
-  ];
+export const appliances = [
+  { value: 50, label: 'Bombilla LED', watts: '50W', image: '/images/bombilla.jpg' },
+  { value: 150, label: 'Portátil', watts: '150W', image: '/images/laptop.jpg' },
+  { value: 300, label: 'Televisor 50"', watts: '300W', image: '/images/tv.jpg' },
+  { value: 800, label: 'Microondas', watts: '800W', image: '/images/microwave.jpg' },
+  { value: 900, label: 'Aire acondicionado', watts: '900W', image: '/images/aire-acondicionado.jpg' },
+  { value: 1200, label: 'Horno', watts: '1.200W', image: '/images/horno.jpg' },
+  { value: 1200, label: 'Secador de pelo', watts: '1.200W', image: '/images/hair-dryer.jpg' },
+  { value: 1800, label: 'Lavaplatos', watts: '1.800W', image: '/images/lavaplatos.jpg' },
+  { value: 2000, label: 'Lavadora', watts: '2.000W', image: '/images/washing-machine.jpg' },
+  { value: 2000, label: 'Radiador eléctrico', watts: '2.000W', image: '/images/radiador-electrico.jpg' },
+  { value: 'custom', label: 'Personalizado', watts: '', image: '/images/personalizado.jpg' },
+];
 
-  export const faqData = [
+export const faqData = [
   {
     question: '¿Cómo calculo el consumo de un electrodoméstico?',
     answer: 'Multiplica la potencia en kW por las horas de uso. Ej.: 1,5 kW × 2 h = 3 kWh.'
